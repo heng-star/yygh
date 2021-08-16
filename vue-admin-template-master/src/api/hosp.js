93aa5f26b7f8fa499186c09bc859402b854524e7
xiaozhi_heng <2895695886@qq.com>
2021-08-16 22:25:50 +0800

    增加挂号前端接口

--- vue-admin-template-master/src/api/hosp.js
+++ vue-admin-template-master/src/api/hosp.js
@@ -2,8 +2,8 @@ import request from '@/utils/request'
 
 export default {
   //医院列表
-  getPageList(current,limit,searchObj) {
-    return request ({
+  getPageList(current, limit, searchObj) {
+    return request({
       url: `/admin/hosp/hospital/findHospList/${current}/${limit}`,
       method: 'get',
       params: searchObj //params参数传递，hospitalQueryVo 
@@ -13,60 +13,76 @@ export default {
   //查询dictCode查询下级数据字典
   findByDictCode(dictCode) {
     return request({
-        url: `/admin/cmn/dict/findChildData/${dictCode}`,
-        method: 'get'
-      })
-    },
-  
+      url: `/admin/cmn/dict/findChildData/${dictCode}`,
+      method: 'get'
+    })
+  },
+
   //根据id查询下级数据字典
   findByParentId(dictCode) {
     return request({
-        url: `/admin/cmn/dict/findChildData/${dictCode}`,
-        method: 'get'
-      })
+      url: `/admin/cmn/dict/findChildData/${dictCode}`,
+      method: 'get'
+    })
   },
   //
 
   //更新状态
-    updateStatusbyid(id, status) {
-        return request({
-            url: `/admin/hosp/hospital/updateHospStatus/${id}/${status}`,
-            method: 'get'
-        })  
-    },
+  updateStatusbyid(id, status) {
+    return request({
+      url: `/admin/hosp/hospital/updateHospStatus/${id}/${status}`,
+      method: 'get'
+    })
+  },
 
-    //查看医院详情
-    getHospById(id) {
-        return request ({
-            url: `/admin/hosp/hospital/showHospDetail/${id}`,
-            method: 'get'
-        })
-    },
+  //查看医院详情
+  getHospById(id) {
+    return request({
+      url: `/admin/hosp/hospital/showHospDetail/${id}`,
+      method: 'get'
+    })
+  },
 
-    //查看医院科室
-    gehoScheduleById(hoscode) {
-      return request ({
-          url: `/admin/hosp/department/getDeptList/${hoscode}`,
-          method: 'get'
-      })  
+  //查看医院科室
+  gehoScheduleById(hoscode) {
+    return request({
+      url: `/admin/hosp/department/getDeptList/${hoscode}`,
+      method: 'get'
+    })
   },
 
   getScheduleRule(page, limit, hoscode, depcode) {
-      return request({
-         url: `/admin/hosp/schedule/getScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
-          method: 'get'
-      })
+    return request({
+      url: `/admin/hosp/schedule/getScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+      method: 'get'
+    })
   },
 
   //根据排版日期时间查询详情
   //查询排班详情
-  getScheduleDetail(hoscode,depcode,workDate) {
-    return request ({
+  getScheduleDetail(hoscode, depcode, workDate) {
+    return request({
       url: `/admin/hosp/schedule/getScheduleDetail/${hoscode}/${depcode}/${workDate}`,
       method: 'get'
     })
-  }
+  },
 
-  
+  //获取挂号规则
+  getBookingScheduleRule(page, limit, hoscode, depcode) {
+    return request({
+      url: `${api_name}/auth/getBookingScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+      method: 'get'
+    })
+  },
+
+  //获取列表
+  findScheduleList(hoscode, depcode, workDate) {
+    return request({
+      url: `${api_name}/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`,
+      method: 'get'
+    })
+  },
+
+
 
 }
--- yygh-site/template/api/hosp/hospital.js
+++ yygh-site/template/api/hosp/hospital.js
@@ -3,33 +3,51 @@ import request from '@/utils/request'
 const api_name = `/api/hosp/hospital`
 
 export default {
- getPageList(page, limit, searchObj) {
-    return request({
-        url: `${api_name}/findHospList/${page}/${limit}`,
-        method: 'get',
-        params: searchObj  //参数
-    })
-  },
+    getPageList(page, limit, searchObj) {
+        return request({
+            url: `${api_name}/findHospList/${page}/${limit}`,
+            method: 'get',
+            params: searchObj  //参数
+        })
+    },
 
- getByHosName(hosname) {
-    return request({
-        url: `${api_name}/findByHosName/${hosname}`,
-        method: 'get'
-    })
-  },
+    getByHosName(hosname) {
+        return request({
+            url: `${api_name}/findByHosName/${hosname}`,
+            method: 'get'
+        })
+    },
 
-  show(hoscode) {
-    return request({
-        url: `${api_name}/findHospDetail/${hoscode}`,
-        method: 'get'
-    })
-},
-findDepartment(hoscode) {
-    return request({
-        url: `${api_name}/department/${hoscode}`,
-        method: 'get'
-    })
-}
+    show(hoscode) {
+        return request({
+            url: `${api_name}/findHospDetail/${hoscode}`,
+            method: 'get'
+        })
+    },
+    findDepartment(hoscode) {
+        return request({
+            url: `${api_name}/department/${hoscode}`,
+            method: 'get'
+        })
+    },
+    getBookingScheduleRule(page, limit, hoscode, depcode) {
+        return request({
+            url: `${api_name}/auth/getBookingScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+            method: 'get'
+        })
+    },
 
+    findScheduleList(hoscode, depcode, workDate) {
+        return request({
+            url: `${api_name}/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`,
+            method: 'get'
+        })
+    },
+    getSchedule(id) {
+        return request({
+            url: `${api_name}/getSchedule/${id}`,
+            method: 'get'
+        })
+    }
 
 }
